a9300fac4627485fe50ca50e202243421e5f31f8
Karanpgowda27 <karanpgowda27@gmail.com>
2025-03-11 17:34:51 +0530

    myfirstcommit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
+
+const App = () => {
+  return (
+    <Grid>
+      <Typography>Surprise motherfucker</Typography>
+      <Typography>Who are you?</Typography>
+      <Typography>Im Karan, Who are YOU?</Typography>
+      <Typography>IM KARAN, Who are YOU?</Typography>
+    </Grid>
+  )
+}
+
+export default App
